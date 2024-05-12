a495f2b93e1e43ac668832c43d84967b6b75c5ca
Fedir Melnyk <pluserwork24@gmail.com>
2024-05-12 14:57:21 +0300

    components (refactor & edit) refactored old verison && added somw styles

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className={'mt-auto flex h-[70px]'}>
+        <footer className={'mt-auto flex h-[70px] bg-black text-white'}>
             <div className={'container mx-auto'}>
                 <p>Footer</p>
             </div>

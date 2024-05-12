4618936d2b3a22faf940268ee398bfb8c2bc0357
Fedir Melnyk <pluserwork24@gmail.com>
2024-05-12 14:56:15 +0300

    add (settup) add some base settup for project && small project refactor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import { Sansita } from 'next/font/google'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'bg_image': "url('../public/bg.png')"
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            purple: '#EAB4F7',
+            yellow: '#F2F99C',
+            black: '#030303',
+            white: '#F1F1F1',
+        },
+        extend: {
+            backgroundImage: {
+                bg_image: "url('../public/images/bg.png')",
+            },
+        },
+        fontFamily: {
+            sans: ['Mont', 'sans-serif'],
+        },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [],
+}
+export default config

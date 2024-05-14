204ed8606fded386a0eea7bf89309750e19a6cc8
Fedir Melnyk <61160727+trusthemind@users.noreply.github.com>
2024-05-14 08:38:01 +0300

    BASE (#2)
    * add some base setup for project && small project refactor
    
    * components (refactor & edit) refactored old verison && added some styles

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,19 +1,21 @@
-import Image from "next/image";
-import logoImage from '@/public/logo.svg';
-import profileImage from '@/public/profile.svg';
-import Link from "next/link";
-import {Navigation} from "@/components/Layout/Navigation";
+import Image from 'next/image'
+import logoImage from '@/public/logo.svg'
+import profileImage from '@/public/profile.svg'
+import Link from 'next/link'
+import { Navigation } from '@/components/Layout/Navigation'
 
 export const Header = () => {
     return (
-        <div className={'container mx-auto h-[70px] flex items-center justify-between relative'}>
-            <Link href={'#'}>
-                <Image src={logoImage} alt={'logo image'} quality={100} height={40}/>
-            </Link>
-            <Navigation/>
-            <Link href={'#'}>
-                <Image src={profileImage} alt={'profile image'} quality={100}/>
-            </Link>
-        </div>
+        <header className={'bg-black flex item-center justify-center p-4'}>
+            <div className={'container flex items-center justify-between'}>
+                <Link href={'#'}>
+                    <Image src={logoImage} alt={'logo image'} quality={100} height={40} />
+                </Link>
+                <Navigation />
+                <Link href={'#'}>
+                    <Image src={profileImage} alt={'profile image'} quality={100} />
+                </Link>
+            </div>
+        </header>
     )
 }

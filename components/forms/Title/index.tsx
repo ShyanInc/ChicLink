55bc930529f42ae66cce274710dc1a774edaa626
Shyan <Shyan.Inc@gmail.com>
2024-06-06 01:25:38 +0300

    Merge dev to master (#6)
    * remove (readme) unnecessary readme txt (#1)
    
    * BASE (#2)
    
    * add some base setup for project && small project refactor
    
    * components (refactor & edit) refactored old verison && added some styles
    
    * . (#3)
    
    * Add Sign In & Sign Up Forms. Modify basic styles
    
    * Create LICENSE
    
    * Fix Form Title with unused vars
    
    ---------
    
    Co-authored-by: Fedir Melnyk <61160727+trusthemind@users.noreply.github.com>

--- components/forms/Title/index.tsx
+++ components/forms/Title/index.tsx
@@ -1,19 +1,11 @@
 'use client'
-import { useRouter } from 'next/navigation'
+
 import { FC } from 'react'
 
 interface TitleProps {
-    backStepRoute?: string
-    backStepText?: string
     text: string
 }
 
-export const FormTitle: FC<TitleProps> = ({ backStepRoute, backStepText, text }) => {
-    const { push } = useRouter()
-    return (
-        <p className={'text-white text-center text-3xl'}>
-            {/*<button onClick={() => push(backStepRoute ?? '/')}>{backStepText}</button>*/}
-            {text && text}
-        </p>
-    )
+export const FormTitle: FC<TitleProps> = ({ text }) => {
+    return <p className={'text-white text-center text-3xl'}>{text && text}</p>
 }

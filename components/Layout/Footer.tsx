a511409e6f0e5e6597955bfb3420ed535e84d055
ShyanInc <shyan.inc@gmail.com>
2024-06-06 02:36:34 +0300

    Modify Footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,10 +1,9 @@
-
 export const Footer = () => {
     return (
         <footer className={'mt-auto flex h-[70px] bg-black text-white'}>
-            <div className={'container mx-auto'}>
-                <p>Footer</p>
-            </div>  
+            <div className={'container mx-auto flex items-center justify-end'}>
+                <p>© 2024 ChicLink</p>
+            </div>
         </footer>
     )
 }
